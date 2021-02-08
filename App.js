5dfd1687623aa5a7b948879a887f9c354aa5894d
danzjamz <djl.hovley@gmail.com>
2021-02-08 14:46:05 -0700

    added/styled input and button for goal adding

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        <TextInput style={{borderBottomColor: 'black', borderBottomWidth: 1, width: '80%'}} />
+        <Button title='ADD' style={styles.btn} />
+      </View>
+      <View></View>
     </View>
   );
 }
@@ -16,6 +19,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: '10px',
+    width: '100%'
+  },
+  input: {
+    borderBottomColor: 'black', borderBottomWidth: 1
+  },
+  btn: {
+    backgroundColor: '#aedf23'
+  }
 });
